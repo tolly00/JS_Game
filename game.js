4cb7a7dd9c232b3e7300d3301be67c6c9d7cb005
tolly00 <albertosylveus@gmail.com>
2020-07-10 13:16:23 -0400

    hide circle

---
+++ game.js
@@ -0,0 +1,9 @@
+var circle=document.getElementById("circle");
+var btn=document.getElementById("btn");
+var score=document.getElementById("score");
+
+btn.addEventListener('click',function(){
+    circle.style.display="none";
+    //alert("Hello");
+});
+
